cd866b44973e605f911b7f51ab3ed65cf0d826b4
Mattia Bonzi <Mattiabonzi@libero.it>
2022-02-14 13:16:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,25 @@
 echo "Init"
 clear
 echo "This is just an installer! \nRefer to the documentation of the single sofwtare for any doubt"
-echo "Script write with <3 by:\n"
-echo "  ______           __  \n /_  __/_  _______/ /_ \n  / / / / / / ___/ __ \\n / / / /_/ / /__/ / / /\n/_/  \__,_/\___/_/ /_/ \n\n"
+echo "Script write with <3 by:\n\n\n"
+echo "  ______           __  \n /_  __/_  _______/ /_ \n  / / / / / / ___/ __ \\n / / / /_/ / /__/ / / /\n/_/  \__,_/\___/_/ /_/ \n\n\n"
 echo "Be sure that you are connected to internet, and taht you're running Android > 6 (api 24)\nFor more info please visit: "
 echo "Update repo"
 pgk update
-pkg upgrade
 echo "Install dependencies"
-pkg install -y python-dev libffi-dev openssl-dev coreutils clang python coreutils nano mosquitto nodejs openssh termux-api make curl
-pip install pynacl==1.3.0
-pip install netdisco
+pkg install -y openssh 
+echo "android" | passwd
+sshd
+pkg install -y  ibffi-dev openssl-dev coreutils clang python3 python3-dev nano mosquitto nodejs openssh termux-api make curl
+pip3 install --upgrade pip
+pip3 install pynacl==1.3.0
+pip3 install netdisco
 echo "Install pm2 and node-red"
 npm i -g --unsafe-perm pm2 node-red
 echo "Install home-assistant"
-python -m venv homeassistant
+python3.9 -m venv homeassistant
 source homeassistant/bin/activate
-pip install homeassistant
+pip3 install homeassistant==2021.9.7
 echo "Install home-assistant configurator"
 cd /data/data/com.termux/files/home/.homeassistant
 curl -LO https://raw.githubusercontent.com/danielperna84/hass-configurator/master/configurator.py
@@ -31,5 +34,8 @@ pm2 start hass --interpreter=python -- --config /data/data/com.termux/files/home
 echo "Start configurator"
 pm2 start /data/data/com.termux/files/home/.homeassistant/configurator.py
 echo "Add Node-RED and Configurator to HASS sidebar"
-echo "panel_iframe:\nconfigurator\n:\ntitle: Configurator\nicon: mdi:wrench\nurl: http://x.x.x.x:3218\nnode_red:\ntitle: Node-RED\nicon: mdi:cogs\nurl: http://x.x.x.x:1880" | /data/data/com.termux/files/home/.homeassistant/configuration.yaml
+echo "\npanel_iframe:\nconfigurator\n:\ntitle: Configurator\nicon: mdi:wrench\nurl: http://x.x.x.x:3218\nnode_red:\ntitle: Node-RED\nicon: mdi:cogs\nurl: http://x.x.x.x:1880" >> /data/data/com.termux/files/home/.homeassistant/configuration.yaml
 echo "Done"
+echo "Use Username: 'admin' and Password: 'android' to connect to the device via ssh on local network (port 8022)"
+echo "To check your ip (may vary) go to (on your phone): 'Setting' => 'Device information' => 'Device status' => 'Ip adress'"
+echo "The ssh command should look like this: 'ssh admin@192.168.x.x -p 8022'"
