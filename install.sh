f8b985e1f5133c14406ec308c7c4b8903f450ffe
Mattia Bonzi <Mattiabonzi@libero.it>
2022-02-15 16:16:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,15 +66,15 @@ echo "Start configurator"
 pm2 start /data/data/com.termux/files/home/.homeassistant/configurator.py
 echo "Add Node-RED and Configurator to HASS sidebar"
 echo "\npanel_iframe:\nconfigurator\n:\ntitle: Configurator\nicon: mdi:wrench\nurl: http://127.0.0.1:3218\nnode_red:\ntitle: Node-RED\nicon: mdi:cogs\nurl: http://127.0.0.1:1880" >> /data/data/com.termux/files/home/homeassistant/configuration.yaml
-echo "\n\n\nDone\n"
-echo "Script write with <3 by:"
-echo "   ______           __  "
-echo "  /_  __/_  _______/ /_ "
-echo "   / / / / / / ___/ __ \ "
-echo "  / / / /_/ / /__  / / / "
-echo " /_/  \__,_/\___/_/ /_/ "
-echo "Use Username: 'admin' and Password: 'android' to connect to the device via ssh on local network (port 8022)"
-echo "To check your ip (may vary) go to (on your phone): 'Setting' => 'Device information' => 'Device status' => 'Ip adress'"
-echo "The ssh command should look like this: 'ssh admin@192.168.x.x -p 8022'"
-echo "You should change yor password now using 'passwd' (once connted via ssh)"
+
+echo -e "\n\n\a\a\aDone\n\nScript write with <3 by:";
+echo -e "   ______           __  "
+echo -e "  /_  __/_  _______/ /_ "
+echo -e "   / / / / / / ___/ __ \ "
+echo -e "  / / / /_/ / /__  / / / "
+echo -e " /_/  \__,_/\___/_/ /_/ \n"
+echo -e "Use Username: 'admin' and Password: 'android' to connect to the device via ssh on local network (port 8022)\n
+To check your ip (may vary) go to (on your phone): 'Setting' => 'Device information' => 'Device status' => 'Ip adress'\n
+The ssh command should look like this: 'ssh admin@192.168.x.x -p 8022'\n
+You should change yor password now using 'passwd' (once connted via ssh)"
 
