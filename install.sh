6cd0c96450772b087f125c38eab60d4db0a880c1
Mattia Bonzi <Mattiabonzi@libero.it>
2022-12-31 17:04:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,22 @@
 clear
 set -e
 
-echo "This is just an installer! \nRefer to the documentation of the Node Red for any doubt"
-echo "Script write with <3 by:"
-echo "   ______           __  "
-echo "  /_  __/_  _______/ /_ "
-echo "   / / / / / / ___/ __ \ "
-echo "  / / / /_/ / /__  / / / "
-echo " /_/  \__,_/\___/_/ /_/ "
-echo "Be sure that you are connected to internet, and taht you're running Android > 6 (api 24)\nFor more info please visit: "
-echo "Use only thermux veriosn downloaded by f-droid (do not use Play-store)"
-echo "Install also Termux api app from f-droid to use the sensor of the phone isnide node-red"
-echo "Install also Termux boot app from f-droid to launch evetything on startup"
-echo "Grant permission when asked!"
+msg() {
+    echo -e "\033[0;32m$1\033[0m"
+}
+
+msg "This is just an installer! \nRefer to the documentation of the Node Red for any doubt"
+msg "Script write with <3 by:"
+msg "   ______           __  "
+msg "  /_  __/_  _______/ /_ "
+msg "   / / / / / / ___/ __ \ "
+msg "  / / / /_/ / /__  / / / "
+msg " /_/  \__,_/\___/_/ /_/ "
+msg "Be sure that you are connected to internet, and taht you're running Android > 6 (api 24)\nFor more info please visit: "
+msg "Use only thermux veriosn downloaded by f-droid (do not use Play-store)"
+msg "Install also Termux api app from f-droid to use the sensor of the phone isnide node-red"
+msg "Install also Termux boot app from f-droid to launch evetything on startup"
+msg "Grant permission when asked!"
 sleep 1
 termux-wake-lock
 sleep 5
@@ -28,20 +32,20 @@ SQUID=
 
 IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 
-echo "Update repo"
+msg "Update repo"
 echo y | pkg install -y wget curl
 curl https://its-pointless.github.io/setup-pointless-repo.sh | bash
 echo y | pkg update
 
-echo "Ask for storage permissionss"
+msg "Ask for storage permissionss"
 
 
-echo "Configure SSH"
+msg "Configure SSH"
 echo y | pkg install openssh
 echo -e "android\nandroid" | passwd
 sshd
 
-echo "Install dependencies"
+msg "Install dependencies"
 #Use a fixed version of python su survive across termux update
 wget https://raw.githubusercontent.com/mattiabonzi/droid-assistant/main/python/python_3.11.1_aarch64.deb
 apt install -y ./python_3.11.1_aarch64.deb
@@ -59,20 +63,21 @@ echo y | pkg install -y binutils
 echo y | pkg install -y ndk-sysroot
 echo y | pkg install -y build-essential
 
-echo "Install pm2"
+msg "Install pm2"
 npm i -g --unsafe-perm pm2
 
 
 if [ -n "$MOSQUITTO" ];then
-    echo "Install and start mosquitto" 
+    msg "Install and start mosquitto" 
     echo y | pkg install mosquitto
     pm2 start mosquitto -- -v -c /data/data/com.termux/files/usr/etc/mosquitto/mosquitto.conf
 fi
 
 if [ -n "$NODERED" ];then
-    echo "Install and  start node-red"
+    msg "Install and  start node-red"
     npm i -g --unsafe-perm node-red
     pm2 start node-red
+    sleep 5
     cd ~/.node-red/
     npm install node-red-dashboard
     npm install node-red-contrib-termux-api
@@ -126,7 +131,11 @@ if [ -n "$HASS" ];then
     pip install cryptography-38.0.3.tar.gz
     pip install -r req.txt homeassistant==2022.12.2
     pip install -I pytz
-    echo "Install home-assistant configurator"
+    msg "Starting hass for the first time, it will takje some time for configuring itself"
+    pm2 start hass --interpreter=python -- --config /data/data/com.termux/files/home/.homeassistant
+    sleep 10m
+    pm2 stop hass
+    msg "Install home-assistant configurator"
     cd /data/data/com.termux/files/home/.homeassistant
     curl -LO https://raw.githubusercontent.com/danielperna84/hass-configurator/master/configurator.py
     chmod 755 configurator.py
@@ -143,22 +152,22 @@ panel_iframe:
 EOF
 )"
     printf "$CONFIG" >> ~/.homeassistant/configuration.yaml
-    pm2 start hass --interpreter=python -- --config /data/data/com.termux/files/home/.homeassistant
+    pm2 restart hass --interpreter=python -- --config /data/data/com.termux/files/home/.homeassistant
     pm2 start /data/data/com.termux/files/home/.homeassistant/configurator.py
-    echo "Hass is installed, it should take some time for configuring itself"
+    msg "Hass is installed, it should take some time for configuring itself"
 fi
 
 if [ -n "$SQUID" ];then
-    echo "Install and start squid" 
+    msg "Install and start squid" 
     echo y | pkg install squid
     pm2 start squid
 fi
 
 
 
-echo "Save pm2 config"
+msg "Save pm2 config"
 pm2 save 
-echo "Add pm2 resurrect to boot file"
+msg "Add pm2 resurrect to boot file"
 [ ! -d ~/.termux/boot/ ] && mkdir -p ~/.termux/boot/
 echo -e "#!/data/data/com.termux/files/usr/bin/sh \ntermux-wake-lock \n. \$PREFIX/etc/profile \nsshd \npm2 start all" > ~/.termux/boot/start.sh
 
@@ -173,13 +182,13 @@ echo -e "#!/data/data/com.termux/files/usr/bin/sh \ntermux-wake-lock \n. \$PREFI
 
 
 
-echo -e "Use Username: 'admin' and Password: 'android' to connect to all servuies, you should change this ASAP!"
-echo -e "Yout IP should be: ${IP}"
-echo -e "Online services:\n"
-echo -e "SSH: port 8022 (use a ssh client) (ssh admin@${IP} -p 8022)\n"
-[ -n "$NODERED" ] && echo -e "NODE-RED: port 1880 (browser) (http://${IP}:1880)\n"
-[ -n "$HASS" ] && echo -e "HASS: port 8123 (browser) (http://${IP}:8123)\n"
-[ -n "$NODERED" ] && echo -e "NODE-RED DASHBOARD: port 1880 (browser) (http://${IP}:1880/ui)\n"
-[ -n "$MOSQUITO" ] && echo -e "MOSQUITO: port 1883 (Mqtt Client)\n"
-[ -n "$SQUID" ] && echo -e "SQUID: port 3128 (Cache proxy)\n"
-echo -e "You should change yor password now using 'passwd' (once connted via ssh)"
+msg "Use Username: 'admin' and Password: 'android' to connect to all servuies, you should change this ASAP!"
+msg "Yout IP should be: ${IP}"
+msg "Online services:\n"
+msg "SSH: port 8022 (use a ssh client) (ssh admin@${IP} -p 8022)\n"
+[ -n "$NODERED" ] && msg "NODE-RED: port 1880 (browser) (http://${IP}:1880)\n"
+[ -n "$HASS" ] && msg "HASS: port 8123 (browser) (http://${IP}:8123)\n"
+[ -n "$NODERED" ] && msg "NODE-RED DASHBOARD: port 1880 (browser) (http://${IP}:1880/ui)\n"
+[ -n "$MOSQUITO" ] && msg "MOSQUITO: port 1883 (Mqtt Client)\n"
+[ -n "$SQUID" ] && msg "SQUID: port 3128 (Cache proxy)\n"
+msg "You should change yor password now using 'passwd' (once connted via ssh)"
